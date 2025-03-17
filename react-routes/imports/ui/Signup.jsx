0e3cf0ee46fa91af3d68a9471c3de7e7179fad1c
Michael Whitney <whitneym@winthrop.edu>
2025-03-16 22:24:45 -0400

    03b - state hook setFormData({...formData, email: e.target.value})}

--- react-routes/imports/ui/Signup.jsx
+++ react-routes/imports/ui/Signup.jsx
@@ -1,31 +1,60 @@
 import React, { useState } from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-
+// functional component
 export const Signup = () => {
 
-  const [error_state, setError] = useState('');
 
-  function submitForm(e){
-    e.preventDefault();   // don't send the form anywhere
+  const [error, setError] = useState('');
+ 
+  // 1. create a formData state object
+  //      this object will be updated whenever
+  //      a user types in the form inputs
+  const [formData_state, setFormData] = useState({
+    email: "",
+    password: ""
+  })
+  // Could have accomplished the same with the following
+  // const [email_state, setEmail] = useState('');
+  // const [password_state, setPassword] = useState('');
 
-    setError('something went wrong');   // will be using down the road
+
+  function submitForm(event){
+    event.preventDefault();   
+// 2: comment out setError as we will be moving that to a method
+    // setError('something went wrong'); 
+
+// 5: test that the state has been updated when user submits the form
+    console.log(formData_state.email + ' ' + formData_state.password);
+
+                                     
   }
 
   return (
     <>
-      <h1>Signup Here!</h1>
-      {/* use the ternary operator ? if error, show error. if no error, show nothing */}
-      {/* this.state.error_state is set to '' which is falsey */}
-      {error_state ? <p>{error_state}</p> : undefined}
+      <h1>Signup here!</h1>
+      {error ? <p>{error}</p> : undefined}
       <form onSubmit={submitForm}>
-        <input type='email' name='email' placeholder='Email' />
-        <input type='password' name='password' placeholder='Password' />
+        <input 
+          type='email' 
+// 3. set the value of the input to the default vaule / anything that has been saved in the state 
+          value={formData_state.email}
+// 4. whenever the input value changes, call the anonymous function to update the formData
+          onChange={(e) => setFormData({...formData_state, email: e.target.value})}
+// Explanation: This takes the event (e) and passes it to the setFormData() function. We can’t just 
+//    set part of the formData, we have to set the full thing. So, we say, take whatever is in the 
+//    form (…formData) and set that and then add the key and value title: e.target.value.
+          placeholder='Email' />
+
+        <input  type='password' 
+                value={formData_state.password} 
+                onChange={(e) => setFormData({...formData_state, password: e.target.value})}
+                placeholder='Password' />
         <button>Create Account</button>
       </form>
       <br /><br />
       <Link to="/">Already have an account?</Link>
-    </>
-  )
-  
-  }
+  </>
+)
+
+}
